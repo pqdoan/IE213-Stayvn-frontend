9f826f5ca44a02d72f3972d8f73f6af1b67aced8
pqd <pqdoan7@gmail.com>
2026-05-08 23:42:26 +0700

    update file hotelService.js

--- src/services/hotelService.js
+++ src/services/hotelService.js
@@ -1,3 +1,30 @@
+/* ========== Hotel Service =========*/
 import api from "./api";
 
-export const getHotels = () => api.get("/hotels");
+const hotelService = {
+  // GET ALL HOTELS
+  getAll: (params) => api.get("/hotels?limit=100", { params }),
+
+  // GET HOTEL DETAIL
+  getById: (id) => api.get(`/hotels/${id}`),
+
+  // CREATE HOTEL
+  create: (data) => api.post("/hotels", data),
+
+  // UPDATE HOTEL
+  update: (data) => api.patch("/hotels", data),
+
+  // UPLOAD HOTEL IMAGES
+  uploadImages: (formData) =>
+    api.patch("/hotels/me/images", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }),
+
+  // DELETE HOTEL IMAGE
+  deleteImage: (publicId) =>
+    api.delete(`/hotels/me/images?public_id=${publicId}`),
+};
+
+export default hotelService;
