27300e3f24b04826f9c9053d50b8f45d5e9d6424
pqd <pqdoan7@gmail.com>
2026-05-10 19:25:20 +0700

    update file AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,50 +1,30 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import MainLayout from "../layouts/MainLayout";
-// import ManagerLayout from "../layouts/ManagerLayout";
-// import AdminLayout from "../layouts/AdminLayout";
-
-// import Home from "../pages/Home";
-// import Hotels from "../pages/Hotels";
-// import HotelDetail from "../pages/HotelDetail";
-// import Booking from "../pages/Booking";
-// import MyBookings from "../pages/MyBookings";
-// import ManagerDashboard from "../pages/ManagerDashboard";
-// import AdminPanel from "../pages/AdminPanel";
-
-// export default function AppRoutes() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         {/* USER */}
-//         <Route element={<MainLayout />}>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/hotels" element={<Hotels />} />
-//           <Route path="/hotel/:id" element={<HotelDetail />} />
-//           <Route path="/booking" element={<Booking />} />
-//           <Route path="/my-bookings" element={<MyBookings />} />
-//         </Route>
-
-//         {/* MANAGER */}
-//         <Route element={<ManagerLayout />}>
-//           <Route path="/manager" element={<ManagerDashboard />} />
-//         </Route>
-
-//         {/* ADMIN */}
-//         <Route element={<AdminLayout />}>
-//           <Route path="/admin" element={<AdminPanel />} />
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
+/* ============================================== */
+/*  APP ROUTERS */
+/* ============================================== */
 
 import { Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ManagerLayout from "../layouts/ManagerLayout";
+import AdminLayout from "../layouts/AdminLayout";
+
 import Home from "../pages/Home";
+import Hotels from "../pages/Hotels";
+import HotelDetail from "../pages/HotelDetail";
+import Booking from "../pages/Booking";
+import MyBookings from "../pages/MyBookings";
+import ManagerDashboard from "../pages/ManagerDashboard";
+import AdminPanel from "../pages/AdminPanel";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<Hotels />} />
+      <Route path="/hotel/:id" element={<HotelDetail />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/my-bookings" element={<MyBookings />} />
+      <Route path="/manager" element={<ManagerDashboard />} />
+      <Route path="/admin" element={<AdminPanel />} />
     </Routes>
   );
 };
