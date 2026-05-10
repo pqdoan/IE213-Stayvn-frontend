8abee2285a65ccc8a4f8541b10ad5836bdfcda3f
pqd <pqdoan7@gmail.com>
2026-05-10 19:23:12 +0700

    update file api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,41 @@
+/* ============================================== */
+/*  API CONFIGURATION */
+/* ============================================== */
+
 import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000, //có timeout 10s, nếu server không phản hồi trong 10s sẽ trả về lỗi
 });
+
+// REQUEST INTERCEPTOR
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+// RESPONSE INTERCEPTOR
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API ERROR:", error);
+
+    // TOKEN EXPIRED
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
