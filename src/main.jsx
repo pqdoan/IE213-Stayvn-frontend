9727d1c1be6cb73be187de75a1f638581417eade
pqd <pqdoan7@gmail.com>
2026-05-17 01:28:47 +0700

    update file main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { HotelProvider } from "./context/HotelContext";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <HotelProvider>
+      <App />
+    </HotelProvider>
   </BrowserRouter>,
 );
